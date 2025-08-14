ac9be574d36ef880bd6ef7561a6a8aed7a024ab7
KimuthuKisal <kimuthukisal23@gmail.com>
2025-08-14 21:01:44 +0530

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ElegantEssence.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
